14d580619bb11c9db5fc60fc575d9bc75a4da3da
Dewanta Rahma Satria <dewantarahmasatria@gmail.com>
2025-03-04 00:39:37 +0700

    tambahan fitur GUI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Narrow", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(112, 177);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 33);
+            label1.TabIndex = 0;
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(647, 92);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "Submit";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(112, 94);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(464, 27);
+            textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
+        private TextBox textBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,21 @@ namespace tp3_103022300071
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string namaPraktikan = textBox1.Text;
+            label1.Text = $"Halo {namaPraktikan}";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
